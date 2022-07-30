1098fc6de655523b995f7487670873f99609af1b
Ferdiansyah <ferdikings03@gmail.com>
2022-07-31 03:30:07 +0700

    DICODING_OPERQTORS

---
+++ asignment operator.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 5
+
+x = x + y;
+
+console.log(x);
+
+/* output
+15
+*/
+
+let x = 10;
+let y = 5
+ 
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
